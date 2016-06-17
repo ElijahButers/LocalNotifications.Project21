be5949e399256506688f2c0c17989610fc77bf79
ElijahButers <Elijahbuters@gmail.com>
2016-06-17 23:00:18 +0300

    Connected two new buttons to ViewController as actions.

--- LocalNotifications.Project21/ViewController.swift
+++ LocalNotifications.Project21/ViewController.swift
@@ -19,7 +19,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func registerLocal(sender: AnyObject) {
+    }
+    
+    @IBAction func scheduleLocal(sender: AnyObject) {
+    }
+    
 }
 
