01f0921b81356c59b5f2d376e22c4b7c10751501
ElijahButers <Elijahbuters@gmail.com>
2016-06-18 11:50:59 +0300

    Added a new method to AppDelegate class where this method can only be called when a notification has definitely been received.

--- LocalNotifications.Project21/AppDelegate.swift
+++ LocalNotifications.Project21/AppDelegate.swift
@@ -26,6 +26,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         return true
     }
+    
+    func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification) {
+        
+        if let userInfo = notification.userInfo {
+            let customField1 = userInfo["CustomField1"] as! String
+            print("didReceiveLocalNotification: \(customField1)")
+        }
+    }
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
